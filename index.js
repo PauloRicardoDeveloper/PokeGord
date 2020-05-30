cb867d6bb48d086cc92483663359b83a8ffe206c
NoPastel <36199423+NoPastel@users.noreply.github.com>
2020-05-30 14:20:41 -0300

    Bora usar Eris
    Eris aparenta ser legal, ent pq n usar?

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 //Import Discord Libs and setup the Client
-var Discord = require("discord.js");
-var client = new Discord.Client();
+var Eris = require("eris");
+var client = new Eris.CommandClient(require("./config.json").token, {}, {prefix: "!", defaultHelpCommand: true});
 
 //Call event Ready to do changes on anything
 client.once("ready", () => {console.log("readyyyyyyyyyy boiiiiiiiii")});
 
+client.on("messageCreate", msg => {
+	if(msg.content == "qui si foda"){
+		msg.channel.createMessage("É isso ai");
+	}
+});
+
+var say = client.registerCommand("say", (msg, args) => {
+	if(args.length == 0){
+		return "Escreveu errado mané";
+	}
+
+	return args.join(" ");
+});
+
+say.registerSubcommand("reverse", (msg, args) => {
+	if(args.length == 0){
+		return "Ta faltando coisa";
+	}
+	return args.join(" ").split("").reverse().join("");
+})
 //Login Discord client
-var configX = require("./config.json");
-client.login(configX.token);
+client.connect();
