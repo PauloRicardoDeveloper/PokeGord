bc3a59d47779d700861a2e63f76cd14f9aee5c49
NoPastel <36199423+NoPastel@users.noreply.github.com>
2020-05-31 13:36:27 -0300

    Inicio e teste da API de Pokedex usando WebScrap
    Adicionada uma função na pasta utils para fazer um webscrap no site: https://pokemondb.net para conseguirmos pegar toda a lista de pokemons disponivel.

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ fs.readdir("./comandos", (err, f) => {
 	require(`./comandos/${f}`);
 })
 
+//Test da pokedex:
+var {pokemonB} = require("./utils/pokedexAPI.js");
+client.on("messageCreate", async msg => {
+	if(msg.content.startsWith("poke")){
+		pokemonB(msg.content.split(" ")[1].toLowerCase()).then(a => {
+			msg.channel.createMessage({
+				embed: {
+					title: "Pokemon Info",
+					description: a.nome,
+					thumbnail: {
+						url: a.img
+					},
+					image: {
+						url: `https://img.pokemondb.net/artwork/${a.nome.toLowerCase()}.jpg`
+					}
+				}
+			})
+		});
+	}
+});
 
 //Login Discord client
 client.connect();
